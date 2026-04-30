afe870a7f597c1b8f235863cf5a030ada02b8cff
Athreya <athreya160@gmail.com>
2026-04-30 21:44:09 +0530

    Added product category field in backend model

--- models/Products.js
+++ models/Products.js
@@ -3,7 +3,14 @@ const mongoose = require("mongoose");
 const productSchema = new mongoose.Schema({
     name: String,
     price: Number,
-    image: String
+    image: String,
+
+    // ✅ NEW FIELD
+    category: {
+        type: String,
+        enum: ["Cashew", "Almond", "Pistachio", "Walnut", "Mixed"],
+        default: "Mixed"
+    }
 });
 
 module.exports = mongoose.model("Product", productSchema);
